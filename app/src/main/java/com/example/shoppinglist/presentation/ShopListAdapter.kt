df4743b6d6ca8732d2cceafc1897e3234f4e6220
milkqurt <diasermekbaev342@gmail.com>
2023-06-25 11:45:05 +0600

    added getItemVieWType

---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.shoppinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
+
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val type = if (viewType == VIEW_TYPE_ENABLED) {
+            R.layout.item_shop_enabled
+        } else {
+            R.layout.item_shop_disabled
+        }
+        val view =
+            LayoutInflater.from(parent.context).inflate(type, parent, false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        holder.tvName?.text = shopItem.name
+        holder.tvCount?.text = shopItem.count.toString()
+        holder.view.setOnLongClickListener {
+            true
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return shopList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val item = shopList[position]
+        return if (item.enabled) {
+            VIEW_TYPE_ENABLED
+        } else {
+            VIEW_TYPE_DISABLED
+        }
+    }
+
+    inner class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tvCount)
+    }
+
+    companion object {
+        const val VIEW_TYPE_ENABLED = 100
+        const val VIEW_TYPE_DISABLED =  101
+    }
+
+}
